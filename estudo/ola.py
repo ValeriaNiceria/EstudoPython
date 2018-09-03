3d4d80ea9301f8ff4b3bb7d946715ba2affdd8e9
ValeriaNiceria <valeriaaniceria@gmail.com>
2018-09-03 08:49:40 -0300

    Iniciando

---
+++ estudo/ola.py
@@ -0,0 +1,23 @@
+print("Hello World")
+
+nome = 'Valéria'
+idade = 25
+print(nome, idade)
+
+nomePergunta = input('Qual o seu nome? ')
+idadePergunta = input('Quantos anos você tem? ')
+print(nomePergunta, idadePergunta)
+
+nomeMensagem = input('Qual o seu nome? ')
+print('Olá ', nomeMensagem, '! Prazer em te conhecer!')
+
+
+dia = input('Dia = ')
+mes = input('Mês = ')
+ano = input('Ano = ')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano)
+
+primeiroNumero = input('Digite um número ')
+segundoNumero = input('Digite outro número ')
+soma = int(primeiroNumero) + int(segundoNumero)
+print('A soma é ', soma)
