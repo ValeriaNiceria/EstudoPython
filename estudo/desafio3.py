e4f9c79937e372b2a4d01232ef21355825bf06fe
ValeriaNiceria <valerianiceria@gmail.com>
2018-09-04 09:40:03 -0300

    Desafio 2 e 3

---
+++ estudo/desafio3.py
@@ -0,0 +1,8 @@
+# Crie um programa que leia dois números mostre a soma entre eles
+
+primeroN = int(input('Digite um número: '))
+segundoN = int(input('Digite outro número: '))
+
+soma = primeroN + segundoN
+
+print('A soma entre {} e  {} é igual a {}!'.format(primeroN, segundoN, soma))
