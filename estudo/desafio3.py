56bdc5be00e77b33413ee9e5d74969bc051aafb3
ValeriaNiceria <valerianiceria@gmail.com>
2018-09-06 09:38:11 -0300

    Desafio 4 - Dissecando variáveis

--- estudo/desafio3.py
+++ estudo/desafio3.py
@@ -1,8 +1,8 @@
 # Crie um programa que leia dois números mostre a soma entre eles
 
-primeroN = int(input('Digite um número: '))
+primeiroN = int(input('Digite um número: '))
 segundoN = int(input('Digite outro número: '))
 
-soma = primeroN + segundoN
+soma = primeiroN + segundoN
 
-print('A soma entre {} e  {} é igual a {}!'.format(primeroN, segundoN, soma))
+print('A soma entre {} e  {} é igual a {}!'.format(primeiroN, segundoN, soma))
