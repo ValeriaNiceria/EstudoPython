97e8d6b5fdfcfe64b757c657bfa893fb8588da0e
ValeriaNiceria <valerianiceria@gmail.com>
2018-09-07 17:12:41 -0300

    Desafio 21

---
+++ estudo/desafio21.py
@@ -0,0 +1,11 @@
+'''
+Faça um programa em python que abra e reproduza
+o áudio de um programa MP3.
+'''
+
+import pygame
+pygame.init()
+
+pygame.mixer.music.load('music.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
