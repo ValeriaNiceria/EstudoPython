cc2ec8b314f183a896286b8fc2e588feb1197bea
ValeriaNiceria <valerianiceria@gmail.com>
2018-09-07 11:49:02 -0300

    Desafio 6

---
+++ estudo/desafio6.py
@@ -0,0 +1,9 @@
+'''
+Crie um algoritmo que leia um número
+e mostre o seu dobro, triplo e raiz quadrada
+'''
+n = int(input('Digite um valor: '))
+print('O número digitado é {}'.format(n))
+print('O dobro é {}'.format(n * 2))
+print('O triplo é {}'.format(n * 3))
+print('A raiz quadrada é {}'.format(pow(n, 2)))
