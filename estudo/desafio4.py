3e6220b810a3f6450debd863f05cf54904d96cc5
ValeriaNiceria <valerianiceria@gmail.com>
2018-09-07 11:11:25 -0300

    Operadores Aritméticos

--- estudo/desafio4.py
+++ estudo/desafio4.py
@@ -1,5 +1,8 @@
-# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
-
+# Anaisando variáveis
+'''
+Faça um programa que leia algo pelo teclado e mostre na tela
+o seu tipo primitivo e todas as informações possíveis sobre ele.
+'''
 variavel = input('Digite algo: ')
 print('O tipo primitivo desse valor é {}'.format(type(variavel)))
 print('Só tem espaços? ', variavel.isspace())
---
+++ estudo/operadores_aritmeticos.py
@@ -0,0 +1,27 @@
+'''
+Operadores Aritméticos 
+'''
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+adicao = n1 + n2
+print('Adição = {}'.format(adicao))
+
+subtracao = n1 - n2
+print('Subtração = {}'.format(subtracao))
+
+multiplicacao = n1 * n2
+print('multiplicação = {}'.format(multiplicacao))
+
+divisao = n1 / n2
+print('Divisão = {}'.format(divisao))
+
+potencia = n1 ** n2
+print('Potência = {}'.format(potencia))
+
+divisao_inteira = n1 // n2
+print('Divisão Inteira = {}'.format(divisao_inteira))
+
+resto_divisao = n1 % n2
+print('Resto da divisão = {}'.format(resto_divisao))
