cb290607bf7f91fc82cef6d600c2d0dd0950cd9c
ValeriaNiceria <valerianiceria@gmail.com>
2018-09-07 13:57:36 -0300

    Desafio 14

---
+++ estudo/desafio14.py
@@ -0,0 +1,7 @@
+'''
+Escreva um programa que converta uma temperatura
+digitada em °C e converta para °F.
+'''
+t = float(input('Informe a temperatura em °C: '))
+f = t * 9 / 5 + 32
+print('A temperatura de {:.1f}°C corresponde a {:.1f}°F!'.format(t, f))
