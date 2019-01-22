91de8885ce1f8808c6d00becfc0e9f024517cbaf
ValeriaNiceria <valerianiceria@gmail.com>
2019-01-22 07:39:56 -0200

    Média Harmônica

--- estatistica/media.py
+++ estatistica/media.py
@@ -1,14 +1,19 @@
-# importando o módulo statistics
+# importando o modulo statistics
 import statistics as stats
 valores = [10, 3, 35, 25, 34, 98, 30, 45, 25, 34, 2]
 
-# Média Aritmética
+# Media Aritmetica
 mediaArit = stats.mean(valores)
-print('Média Aritmética: {:.2f}'.format(mediaArit))
+print('Media Aritmetica: {:.2f}'.format(mediaArit))
 
-# Média Geométrica
+# Media Geometrica
 from functools import reduce
 produtoLista = reduce(lambda x, y: x*y, valores)
 tamanhoLista = len(valores)
 mediaGeo = produtoLista ** (1 / tamanhoLista)
-print('Média Geométrica: {:.2f}'.format(mediaGeo))
+print('Media Geometrica: {:.2f}'.format(mediaGeo))
+
+
+# Media Harmonica
+mediaHarm = stats.harmonic_mean(valores)
+print('Media Harmonica: {:.2f}'.format(mediaHarm))
