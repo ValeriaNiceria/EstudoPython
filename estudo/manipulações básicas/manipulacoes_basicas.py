fad75531ec6229a74b06b5860e2f7c76cae56547
ValeriaNiceria <valerianiceria@gmail.com>
2019-01-25 21:53:56 -0200

    Operadores lógicos

--- estudo/manipulações básicas/manipulacoes_basicas.py
+++ estudo/manipulações básicas/manipulacoes_basicas.py
@@ -48,3 +48,15 @@ valor_imposto
     
 
 
+# Operadores lógicos
+imposto = float(input('Imposto: '))
+if imposto < 10:
+    print('Baixo')
+elif imposto >= 10 and imposto <= 27:
+    print('Médio')
+elif imposto > 27 and imposto < 100:
+    print('Alto')
+else:
+    print('Imposto inválido')
+
+
