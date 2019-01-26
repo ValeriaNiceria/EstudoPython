8f73e88927fe6d95d779bcc5bf3b9f3e36d1f0db
ValeriaNiceria <valerianiceria@gmail.com>
2019-01-25 23:25:03 -0200

    Loop com while

--- estudo/manipulações básicas/manipulacoes_basicas.py
+++ estudo/manipulações básicas/manipulacoes_basicas.py
@@ -60,3 +60,27 @@ else:
     print('Imposto inválido')
 
 
+# Loop com while
+salario = int(input('Salario? '))
+imposto = 27
+while imposto > 0:
+    imposto = input('Imposto ou (0) para sair: ')
+    if not imposto:
+        imposto = 27
+    else:
+        imposto = float(imposto)
+    print('Valor real: {0}'.format(salario - (salario * imposto * 0.01)))
+
+
+# Loop com while (break)
+salario = int(input('Salario? '))
+imposto = 27
+while imposto > 0:
+    imposto = input('Imposto ou (s) para sair: ')
+    if not imposto:
+        imposto = 27
+    elif imposto == 's':
+        break
+    else:
+        imposto = float(imposto)
+    print('Valor real: {0}'.format(salario - (salario * imposto * 0.01)))
