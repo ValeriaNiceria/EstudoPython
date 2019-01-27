52579faa9354fe9a97b22aa0509ad03411c69be9
ValeriaNiceria <valerianiceria@gmail.com>
2019-01-27 12:43:12 -0200

    Declarando Funções: Comando DEF

---
+++ estudo/funções/comando_def.py
@@ -0,0 +1,6 @@
+# Declarando Funções: Comando DEF
+# Funções são objetos de primeira classe (first class objects)
+def sum(a, b):
+    return a + b
+c = sum(1, 2)
+print('A soma dos valores é igual {0}'.format(c))
