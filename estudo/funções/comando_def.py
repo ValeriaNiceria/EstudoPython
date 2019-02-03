6b955f85dac4605bc11e4f005de08ccd73b92b94
ValeriaNiceria <valerianiceria@gmail.com>
2019-02-03 18:04:02 -0200

    update

--- estudo/funções/comando_def.py
+++ estudo/funções/comando_def.py
@@ -1,6 +1,12 @@
 # Declarando Funções: Comando DEF
 # Funções são objetos de primeira classe (first class objects)
-def sum(a, b):
+def soma(a, b):
     return a + b
-c = sum(1, 2)
-print('A soma dos valores é igual {0}'.format(c))
+c = soma(1, 2)
+print('A soma dos valores é igual', c)
+
+# Valores padronizados de argumentos
+def salario_descontado_imposto(salario, imposto=27):
+    return salario - (salario * imposto * 0.01)
+
+print('Salario descontando imposto: ', salario_descontado_imposto(1500))
