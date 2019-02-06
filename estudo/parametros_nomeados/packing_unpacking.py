fa2209f99f77869cb773d68e95215859badef7a8
ValeriaNiceria <valerianiceria@gmail.com>
2019-02-06 21:58:29 -0200

    Unpacking

--- estudo/parametros_nomeados/packing_unpacking.py
+++ estudo/parametros_nomeados/packing_unpacking.py
@@ -32,3 +32,20 @@ new_user(**config) # neste caso, a sintaxe é **dicionário
 dentro da função, e não na chamada
 ----------------------------------
 '''
+def unpacking_experiment(*args):
+    arg1 = args[0]
+    arg2 = args[1]
+    others = args[2:]
+    print(arg1)
+    print(arg2)
+    print(others)
+
+# Usando
+unpacking_experiment(1, 2, 3, 4, 5, 6, 7)
+
+
+def unpacking_experiment2(**kwargs):
+    print(kwargs)
+
+# Usando
+unpacking_experiment2(name="Teste", other="Other")
