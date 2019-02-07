095cb845bfe6172d1a393367b3f2791f674636a4
ValeriaNiceria <valerianiceria@gmail.com>
2019-02-07 09:04:30 -0200

    Laço - for

---
+++ estudo/laços/for/soma.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  7 09:00:35 2019
+
+@author: valeria
+"""
+
+# Soma de 0 a 99
+s = 0
+for x in range(1, 100): # Função 'range' range(comecando, menoresQue, passos)
+    s += x
+print('Total soma:', s)
