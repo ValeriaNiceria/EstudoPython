ad018b8850c23080f5d45f75c0a7332d8955b51d
ValeriaNiceria <valerianiceria@gmail.com>
2019-02-07 08:32:14 -0200

    lista

---
+++ estudo/estrutura de dados/lista/instrumentos_musicais.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  7 08:31:22 2019
+@author: valeria
+"""
+
+# Uma lista de instrumentos musicais
+intrumentos = ['Violão', 'Teclado', 'Bateria', 'Guitarra']
+
+# Para cada nome na lista de instrumentos
+for intrumento in intrumentos:
+    # mostra o nome do intrumento musical
+    print(intrumento)
+
+
