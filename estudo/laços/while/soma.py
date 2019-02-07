71d78033e37fb9a94eb31a382621f8bf02ff1f0d
ValeriaNiceria <valerianiceria@gmail.com>
2019-02-07 09:07:39 -0200

    Laço while

---
+++ estudo/laços/while/soma.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  7 09:05:32 2019
+
+@author: valeria
+"""
+
+# Soma de 0 a 99
+s = 0
+x = 1
+while x < 100:
+    s += x
+    x += 1
+print('Total soma:', s)
