a7751c17a4e99ea33eed18af3b1fa7d0f1845af7
ValeriaNiceria <valerianiceria@gmail.com>
2019-02-07 08:39:18 -0200

    resto divisão

--- estudo/estrutura de dados/lista/instrumentos_musicais.py
+++ estudo/estrutura de dados/lista/instrumentos_musicais.py
@@ -7,10 +7,7 @@ Created on Thu Feb  7 08:31:22 2019
 
 # Uma lista de instrumentos musicais
 intrumentos = ['Violão', 'Teclado', 'Bateria', 'Guitarra']
-
 # Para cada nome na lista de instrumentos
 for intrumento in intrumentos:
     # mostra o nome do intrumento musical
     print(intrumento)
-
-
---
+++ estudo/estrutura de dados/lista/resto_divisao.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  7 08:36:13 2019
+@author: valeria
+"""
+
+# Para i na lista 234, 654, 378, 798:
+for i in [234, 654, 378, 798]:
+    # Se o resto dividindo por 3 for igual a zer:
+    if i % 3 == 0:
+        # Imprime...
+        print(i, '/ 3 =', i / 3)
