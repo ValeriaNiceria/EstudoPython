f0bf2a1dedac9389fe57fde8a0f60ad52c993e2a
ValeriaNiceria <valerianiceria@gmail.com>
2019-04-03 08:43:59 -0300

    update

--- web scraping/main.py
+++ web scraping/main.py
@@ -39,15 +39,19 @@ for i in range(len(links_lista)):
 
 	titulo = titulos_lista[i]
 	titulo = titulo.replace('.', '-')
+	titulo = titulo.replace('/', '-')
+	print('Título:', titulo)
 
 	soup = BeautifulSoup(req.text, 'lxml')
 
 	descricacao = soup.find(class_ = "embedded-content").p.text
-
 	info = soup.find_all("td", class_ = "dataset-details");
 	
 	informacoes = {
-		'informações' : {
+		'titulo': titulo,
+		'descricacao': descricacao,
+		'link': url,
+		'info_adicional': {
 			'autor': info[0].next_element,
 			'mantenedor': info[1].next_element,
 			'cobertura_geografica': info[2].next_element,
@@ -59,28 +63,78 @@ for i in range(len(links_lista)):
 		}
 	}
 
-	print('informações', type(informacoes))
+	# Enviando os dados para o Firebase
+	# db.child(titulo).set(informacoes)
+
 
 	resource_item = soup.find("li", class_ = "resource-item")
 	csv_url = resource_item.find(class_ = "resource-url-analytics")["href"]
-
 	csv = requests.get(csv_url).content
-
 	df = pd.read_csv(io.StringIO(csv.decode('latin1')), error_bad_lines=False, sep=',')
 
-	tipos = df.dtypes 
+	colunas = df.columns.values
 
-	print('Tipos:', tipos)
-	print('Names:', df.names)
-	
+	for coluna in colunas:
 
-	# Enviando os dados para o Firebase
-	#results = db.child(titulo).push(tipos_json)
+		if (df[coluna].dtype == 'float64'):
+			# Se o tipo da coluna for float
+			print('Describe:', df[coluna].describe().transpose().head())
+			desc_index = df[coluna].describe().index
+			desc_value = df[coluna].describe().tolist()
 
-	# print('Título:', titulo)
-	#print('DataFrame:', type(df.dtypes))
-	#print('Results Firebase:', results)
-	#print('Cabeçalho:', df.head(2))
+			data_coluna = {}
+			for d_i in range(len(desc_index)):
+				data_coluna[desc_index[d_i]] = desc_value[d_i]
+
+			coluna = coluna.replace('.', '-')
+			coluna = coluna.replace('/', '-')
+			dados_coluna = {
+				'coluna': coluna,
+				'tipo': 'float',
+				'dados_coluna': data_coluna
+			}
+
+			# Enviando os dados para o Firebase
+			# db.child(titulo + '/variaveis/' + coluna).set(dados_coluna)
+		elif (df[coluna].dtype == 'int64'):
+			# Se o tipo da coluna for inteiro
+			desc_index = df[coluna].describe().index
+			desc_value = df[coluna].describe().tolist()
+
+			data_coluna = {}
+			for d_i in range(len(desc_index)):
+				data_coluna[desc_index[d_i]] = desc_value[d_i]
+
+			coluna = coluna.replace('.', '-')
+			coluna = coluna.replace('/', '-')
+			dados_coluna = {
+				'coluna': coluna,
+				'tipo': 'int',
+				'dados_coluna': data_coluna
+			}
+
+			# Enviando os dados para o Firebase
+			# db.child(titulo + '/variaveis/' + coluna).set(dados_coluna)
+		elif (df[coluna].dtype == 'object'):
+			# print('Tipo:', df[coluna].dtype)
+			# print('*** # String # ***')
+			# print('Coluna:', coluna)
+			# print('Count:', df[coluna].value_counts())
+
+			print('*** # object # ***')
+			# valores = df[coluna].astype('float64', errors='ignore')
+			# print('Index:', valores.value_counts().index)
+			# print('Valor', valores.describe().tolist())
+
+			# dados_coluna = {}
+			# for d_i in range(len(desc_index)):
+			# 	dados_coluna[desc_index[d_i]] = desc_value[d_i]
+
+			# dados = {
+			# 	'tipo': df[coluna].dtype,
+			# 	'dados_coluna': dados_coluna
+			# }
+			# print('dados:', dados)
 
 #print(links_lista)
 #print(titulos_lista)
